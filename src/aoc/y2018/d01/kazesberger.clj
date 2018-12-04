3ff8095a228c46a08e1ae2ff320d071fddcbc078
kazesberger <klaus.azesberger@gmail.com>
2018-12-04 23:20:33 +0100

    solve y18d02

---
+++ src/aoc/y2018/d01/kazesberger.clj
@@ -0,0 +1,65 @@
+(ns aoc.y2018.d01.kazesberger
+  (:require [clojure.string :as str]))
+
+(def puzzle-input (map read-string (str/split (slurp "resources/puzzle-input/y18d01") #"\s+")))
+
+(reduce + 0 puzzle-input)
+
+(count puzzle-input)
+
+
+
+(comment
+  (reductions + 0 puzzle-input)
+
+  (defn first-duplicate [seenset x]
+    (if (seenset x)
+      (reduced x)
+      (conj seenset x)))
+
+  (reduce first-duplicate #{} (reductions + 0 (cycle puzzle-input)))
+
+  (nth (cycle puzzle-input) 977)
+  (second (cycle puzzle-input)))
+
+;(take-while #(not= 3 %) (range)))
+
+(comment)
+;(second
+;  ((fn part2
+;     ([] (part2 '(0) (reductions + 0 puzzle-input)))
+;     ([to from]
+;      (lazy-seq
+;        (let   [num (first from)
+;                iteration (rest (if (< (last to) num) (iterate inc num) (iterate dec num)))]
+;          ;(range)))))))
+;          (part2 (concat to (take-while #(not= num %) iteration))
+;                 (rest from)))))))))
+
+
+;(let   [to '(0)
+;        from '(5 -5 10 -10)]
+;
+;  (loop  [num (first from)
+;          iteration (rest (if (< (last to) num) (iterate inc (last to)) (iterate dec (last to))))]
+;    (recur (concat to (take-while #(not= % num) iteration) (list num))
+;           (rest from))))
+;
+;(let   [to '(0)
+;        from '(5 -5 10 -10)
+;        num (first from)
+;        iteration (rest (if (< (last to) num) (iterate inc (last to)) (iterate dec (last to))))]
+;  (concat to (take-while #(not= num %) iteration) (list num)))
+;
+;(let   [to '(0 1 2 3 4 5)
+;        from (rest '(5 -5 10 -10))
+;        num (first from)
+;        iteration (rest (if (< (last to) num) (iterate inc (last to)) (iterate dec (last to))))]
+;  (concat to (take-while #(not= num %) iteration) (list num)))
+;
+;(take 3 (iterate dec 0))
+;(concat '(0 1 2 3) '(2 1) (list 0)))
+;
+
+
+
---
+++ src/aoc/y2018/d02/kazesberger.clj
@@ -0,0 +1,25 @@
+(ns aoc.y2018.d02.kazesberger
+  (:require [clojure.string :as str]))
+
+(def puzzle-input (str/split (slurp "resources/puzzle-input/y18d02") #"\s+"))
+
+(map (partial group-by identity) puzzle-input)
+(vals (map frequencies puzzle-input))
+
+(->> puzzle-input
+     (map frequencies)
+     (map vals)
+     (filter (partial some #{3}))
+     (count))
+(* 248 25)
+
+
+(str/join (apply map #(when (= %1 %2) %1) (first (for [box1 puzzle-input
+                                                       box2 puzzle-input
+                                                       :when (and (not= box1 box2)
+                                                                  (= 1 (count (filter true? (map not= box1 box2)))))]
+                                                   [box1 box2]))))
+
+
+(filter #(some (> (count (second %)) 2)%) (group-by identity "xpysotkqubuhefscajodiglvzw"))
+
