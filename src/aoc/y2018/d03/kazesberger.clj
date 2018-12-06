b06337bbaf361640d24bbec593cca4317259af02
kazesberger <klaus.azesberger@gmail.com>
2018-12-06 03:07:25 +0100

    part2 took me wayyy too long xD

--- src/aoc/y2018/d03/kazesberger.clj
+++ src/aoc/y2018/d03/kazesberger.clj
@@ -23,14 +23,43 @@
                                                         [a b]))
                                                     puzzle-input)))))
 
-(let [claims (map
+
+(let [claims (take 10 (map
+                        (fn [{:keys [id x y width height]}]
+
+                          {id (set (for [a (range (inc x) (+ (inc x) width))
+                                         b (range (inc y) (+ (inc y) height))]
+                                     [a b]))})
+                        puzzle-input))]
+  ; into clean-claims #(remove overlaps) claims
+  (reduce))
+
+(comment
+  (def claims-processed {:valid-claims #{1 2 3}
+                         :processed #{[1 1] [2 2]}})
+  (fn [{:keys [valid-claims processed]} [id claimed-fields]])
+
+  (fn [{:keys [field #{ids}]} [id claimed-fields]])
+
+  (def claims {
+               1 #{[1 1]
+                   [2 2]}
+               2 #{[3 3]
+                   [4 4]}
+               3 #{[2 2]
+                   [5 5]}}))
+
+(let [claims (mapcat
                (fn [{:keys [id x y width height]}]
 
-                 {id (set (for [a (range (inc x) (+ (inc x) width))
-                                b (range (inc y) (+ (inc y) height))]
-                            [a b]))})
+                 (map #(vector id %) (for [a (range (inc x) (+ (inc x) width))
+                                           b (range (inc y) (+ (inc y) height))]
+                                       [a b])))
                puzzle-input)]
-  (group-by second claims))
+  ;(take 100 claims))
+  ;(filter (set (range 2 1000)) (map (comp count val) (group-by second (take 100000 claims))))
+  (clojure.set/difference (set (map :id puzzle-input)) (set (mapcat #(map first %) (vals (filter #(< 1 ((comp count val) %)) (group-by second claims)))))))
+  ;(count (filter #(< 1 ((comp count val) %)) (group-by second claims))))
 
   ;(for [c1 claims
   ;      c2 claims
