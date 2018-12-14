f407973a78d785b9839cd05687f3c8d26b6d5f88
kazesberger <klaus.azesberger@gmail.com>
2018-12-15 00:10:17 +0100

    Solve y18d08-p1

--- src/aoc/y2018/d08/kazesberger.clj
+++ src/aoc/y2018/d08/kazesberger.clj
@@ -16,8 +16,8 @@
 ;         (drop-last metadata-count)
 ;         (take-child-nodes child-count))))
 
-(defn get-metadata [node]
-  (take-last (second node) node))
+(defn get-metadata-of-childless-node [node]
+  (take (second node) (drop 2 node)))
 
 ;(defn flatten-nodes [node agg]
 ;  (if (not (zero? (first node)))
@@ -49,7 +49,7 @@
   (println "take-childless")
   (let [childless-child-node (take (+ 2 (nth node 3)) (drop 2 node))]
     (-> layer
-        (update :parsed-nodes #(conj % (->Node (swap! next-id inc) id (get-metadata childless-child-node))))
+        (update :parsed-nodes #(conj % (->Node (swap! next-id inc) id (get-metadata-of-childless-node childless-child-node))))
         (update :node #(concat [(dec (first %1)) (second %1)] (drop (+ 2 (count childless-child-node)) %1))))))
     ;{:parsed-nodes (conj parsed-nodes (->Node (swap! next-id inc) id (get-metadata childless-child-node)))
     ; :node         (concat [(dec (first node)) (second node)] (drop (+ 2 (count childless-child-node)) node))
@@ -60,21 +60,48 @@
 
 
 (defn drill-down [{:keys [node id parsed-nodes pid] :as layer}]
-  (println (dissoc layer :node))
-  (if (zero? (first node))
-    (let [leaf-node-numbers (take (+ 2 (second node)) node)
-          rest (drop (count leaf-node-numbers) node)]
-      {:node rest
-       :parsed-nodes (conj parsed-nodes (->Node id pid (get-metadata leaf-node-numbers)))
-       :id pid}) ;
+  ;(println (dissoc layer :node))
+  ;(println layer)
+  (loop [node node
+         pid pid
+         parsed-nodes parsed-nodes]
+    ;(println "drilling down: " (first node) "children left.")
+    ;(println "pn: " parsed-nodes)
+    (if (zero? (first node))
+      (let [leaf-node-numbers (take (+ 2 (second node)) node)
+            rest (drop (count leaf-node-numbers) node)]
+        ;(println "got no children left - i'm leaf: " leaf-node-numbers)
+        ;(println "got no children left - i'm rest: " rest)
+        {:node rest
+         :parsed-nodes (conj parsed-nodes (->Node id pid (get-metadata-of-childless-node leaf-node-numbers)))
+         :id pid})
+      (let [rest-layer-without-1st-child
+            (drill-down {:node         (drop 2 node)
+                         :id           (swap! next-id inc)
+                         :pid          id
+                         :parsed-nodes parsed-nodes})]
 
-    (if (zero? (nth node 2))
-      (drill-down (take-childless-node layer))
-      (drill-down {:node (drop 2 node) :id (swap! next-id inc) :pid id}))))
+          (recur (concat [(dec (first node)) (second node)] (:node rest-layer-without-1st-child))
+                 pid
+                 (:parsed-nodes rest-layer-without-1st-child))))))
 
-(drill-down {:parsed-nodes [] :node [1 1 0 2 10 10 20] :id (reset! next-id 1) :pid -1})
-(drill-down {:parsed-nodes [] :node pinput :id (reset! next-id 1) :pid -1})
+(defn part-1 []
+  (let [pns (:parsed-nodes (drill-down {:parsed-nodes [] :node pinput :id (reset! next-id 1) :pid -1}))]
+    (apply + (mapcat :metadata pns))))
 
+(part-1)
+
+
+(comment
+  (get-metadata-of-childless-node '(0 1 40))
+  (drill-down {:parsed-nodes [] :node [2 1 0 1 99 2 2 0 1 30 0 1 40 20 20 10] :id (reset! next-id 1) :pid -1})
+  (drill-down {:parsed-nodes [] :node pinput :id (reset! next-id 1) :pid -1}))
+
+(comment
+      {:node         (concat [(dec (first node)) (second node)] (:node rest-layer-without-1st-child))
+       :parsed-nodes (:parsed-nodes rest-layer-without-1st-child)
+       :id           id
+       :pid          pid})
 
 ;(defn drill-down
 ;  [{:keys [node id pid] :as node-layer} [:as parsed-nodes]]
@@ -108,6 +135,6 @@
 
 (comment
   (get-children [0 2 1 2 3 4 5])
-  (get-metadata [0 2 1 2 3 4 5])
+  (get-metadata-of-childless-node [0 2 1 2 3 4 5])
   (get-children [2 2 1 2 3 4 5])
-  (get-metadata [2 2 1 2 3 4 5]))
+  (get-metadata-of-childless-node [2 2 1 2 3 4 5]))
